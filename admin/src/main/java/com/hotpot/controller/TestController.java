bfe37c9e6eb4b1b69cbfdd38fda8bf046e4694d6
peng.zou <peng.zou@dmall.com>
2015-12-25 16:58:56 +0800

    add static file for page

---
+++ admin/src/main/java/com/hotpot/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.hotpot.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zoupeng on 15/12/25.
+ */
+@Controller("/test/")
+public class TestController {
+}
