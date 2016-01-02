8db9965257262aa129e2b31863872570ef021af1
MissKidding <cj@chenmatoMacBook-Pro.local>
2016-01-02 21:48:45 +0800

    add js

--- admin/src/main/java/com/hotpot/controller/TestController.java
+++ admin/src/main/java/com/hotpot/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.hotpot.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,10 +9,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by zoupeng on 15/12/25.
  */
 @Controller
+@RequestMapping("/test/")
 public class TestController {
     @RequestMapping("")
     @ResponseBody
     public String test(){
-        return "okay;";
+        return "index";
+    }
+    @RequestMapping("order")
+    public String order(){
+        return "order";
     }
 }
+
