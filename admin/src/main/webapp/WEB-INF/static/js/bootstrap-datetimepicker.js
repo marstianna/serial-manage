b83ed712d730c59e821bd0e77a4dbf5ec0880a8f
MissKidding <cj@chenmatoMacBook-Pro.local>
2016-01-04 22:18:14 +0800

    commit

--- admin/src/main/webapp/WEB-INF/static/js/bootstrap-datetimepicker.js
+++ admin/src/main/webapp/WEB-INF/static/js/bootstrap-datetimepicker.js
@@ -1365,7 +1365,27 @@
       return this;
   };
 
+  //$.fn.datetimepicker.defaults = {
+  //};
   $.fn.datetimepicker.defaults = {
+    autoclose: false,
+    beforeShowDay: $.noop,
+    calendarWeeks: false,
+    clearBtn: false,
+    daysOfWeekDisabled: [],
+    endDate: Infinity,
+    forceParse: true,
+    format: 'mm/dd/yyyy',
+    keyboardNavigation: true,
+    language: 'cn',
+    minViewMode: 0,
+    orientation: "auto",
+    rtl: false,
+    startDate: -Infinity,
+    startView: 0,
+    todayBtn: false,
+    todayHighlight: false,
+    weekStart: 0
   };
   $.fn.datetimepicker.Constructor = Datetimepicker;
   var dates = $.fn.datetimepicker.dates = {
@@ -1377,7 +1397,18 @@
       monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       meridiem:    ['am', 'pm'],
       suffix:      ['st', 'nd', 'rd', 'th'],
-      today:       'Today'
+      today: "Today",
+      clear: "Clear"
+    },
+    cn: {
+      days: ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+      daysShort: ["日", "一", "二", "三", "四", "五", "六", "七"],
+      daysMin: ["日", "一", "二", "三", "四", "五", "六", "七"],
+      months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+      monthsShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+      meridiem:    ['shan', 'pm'],
+      today: "今天",
+      clear: "清除"
     }
   };
 
