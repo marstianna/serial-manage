438286540c94c24ca4dafba86dab101406cd4539
MissKidding <cj@chenmatoMacBook-Pro.local>
2016-01-05 10:20:28 +0800

    more

--- admin/src/main/webapp/WEB-INF/static/js/bootstrap-datetimepicker.js
+++ admin/src/main/webapp/WEB-INF/static/js/bootstrap-datetimepicker.js
@@ -1389,24 +1389,24 @@
   };
   $.fn.datetimepicker.Constructor = Datetimepicker;
   var dates = $.fn.datetimepicker.dates = {
-    en: {
-      days:        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-      daysShort:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      daysMin:     ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-      months:      ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      meridiem:    ['am', 'pm'],
-      suffix:      ['st', 'nd', 'rd', 'th'],
-      today: "Today",
-      clear: "Clear"
-    },
+    //en: {
+    //  days:        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    //  daysShort:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    //  daysMin:     ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+    //  months:      ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    //  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    //  meridiem:    ['am', 'pm'],
+    //  suffix:      ['st', 'nd', 'rd', 'th'],
+    //  today: "Today",
+    //  clear: "Clear"
+    //},
     cn: {
       days: ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
       daysShort: ["日", "一", "二", "三", "四", "五", "六", "七"],
       daysMin: ["日", "一", "二", "三", "四", "五", "六", "七"],
       months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
       monthsShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-      meridiem:    ['shan', 'pm'],
+      meridiem:    ['上午', '下午'],
       today: "今天",
       clear: "清除"
     }
