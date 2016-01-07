4de8103277487c6cca2aff6c694919a560ca09d4
MissKidding <cj@chenmatoMacBook-Pro.local>
2016-01-07 23:24:44 +0800

    pull

--- admin/src/main/webapp/WEB-INF/static/js/bootstrap-datetimepicker.js
+++ admin/src/main/webapp/WEB-INF/static/js/bootstrap-datetimepicker.js
@@ -1365,50 +1365,27 @@
       return this;
   };
 
-  //$.fn.datetimepicker.defaults = {
-  //};
   $.fn.datetimepicker.defaults = {
-    autoclose: false,
-    beforeShowDay: $.noop,
-    calendarWeeks: false,
-    clearBtn: false,
-    daysOfWeekDisabled: [],
-    endDate: Infinity,
-    forceParse: true,
-    format: 'mm/dd/yyyy',
-    keyboardNavigation: true,
-    language: 'cn',
-    minViewMode: 0,
-    orientation: "auto",
-    rtl: false,
-    startDate: -Infinity,
-    startView: 0,
-    todayBtn: false,
-    todayHighlight: false,
-    weekStart: 0
   };
   $.fn.datetimepicker.Constructor = Datetimepicker;
   var dates = $.fn.datetimepicker.dates = {
-    //en: {
-    //  days:        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    //  daysShort:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    //  daysMin:     ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-    //  months:      ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    //  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    //  meridiem:    ['am', 'pm'],
-    //  suffix:      ['st', 'nd', 'rd', 'th'],
-    //  today: "Today",
-    //  clear: "Clear"
-    //},
-    cn: {
-      days: ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-      daysShort: ["日", "一", "二", "三", "四", "五", "六", "七"],
-      daysMin: ["日", "一", "二", "三", "四", "五", "六", "七"],
+    en: {
+      //days:        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      //daysShort:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      //daysMin:     ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+      //months:      ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      //monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      //meridiem:    ['am', 'pm'],
+      //suffix:      ['st', 'nd', 'rd', 'th'],
+      //today:       'Today'
+      days: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"],
+      daysShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+      daysMin:  ["日", "一", "二", "三", "四", "五", "六", "日"],
       months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
       monthsShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-      meridiem:    ['上午', '下午'],
-      today: "今天",
-      clear: "清除"
+      today: "今日",
+      suffix: [],
+      meridiem: ["上午", "下午"]
     }
   };
 
