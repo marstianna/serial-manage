2c657413bf5f604b3087684e8c94648f6688508d
peng.zou <peng.zou@dmall.com>
2016-01-08 23:34:05 +0800

    comment filter config

--- admin/src/main/java/com/hotpot/filter/AuthFilter.java
+++ admin/src/main/java/com/hotpot/filter/AuthFilter.java
@@ -20,6 +20,7 @@ public class AuthFilter implements Filter{
 
     }
 
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
