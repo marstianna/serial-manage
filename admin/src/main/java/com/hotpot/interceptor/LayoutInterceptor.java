d7eeaf199ea779689990127f8be9d2d4590dc8bc
peng.zou <peng.zou@dmall.com>
2016-01-15 13:44:44 +0800

    fix page bugs

--- admin/src/main/java/com/hotpot/interceptor/LayoutInterceptor.java
+++ admin/src/main/java/com/hotpot/interceptor/LayoutInterceptor.java
@@ -39,7 +39,8 @@ public class LayoutInterceptor implements HandlerInterceptor {
             }
             if (queryString.length() > 0) {
                 String result = queryString.substring(0, queryString.length() - 1);
-                request.setAttribute("requestURL", request.getRequestURL() + "?" + result + "&useLayout=false");
+//                request.setAttribute(USE_LAYOUT, false);
+                request.setAttribute("requestURL", request.getRequestURL() + "?" + result+"&useLayout=false");
             }
         }
     }
