9344f2f070e0861eb51a63d41e42d55ef9f9c4c5
chenkunyun <635448711@qq.com>
2016-01-15 21:22:56 +0800

    1. go fmt 2. new write uuid

--- wsr-reader/src/server/httpserver.go
+++ wsr-reader/src/server/httpserver.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"errors"
 
 	"wsr"
 )
@@ -12,6 +11,7 @@ import (
 func Run(addr string) error {
 	http.HandleFunc("/", defaultPage)
 	http.HandleFunc("/read_card", readCard)
+	http.HandleFunc("/write_card", writeCard)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		return err
 	}
@@ -20,7 +20,7 @@ func Run(addr string) error {
 }
 
 func defaultPage(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, newReplyFail(errors.New("wrong page")).toJsonString())
+	io.WriteString(w, newReplyFailWithString("wrong page").toJsonString())
 }
 
 //读取卡号和uuid
@@ -71,4 +71,51 @@ func readCard(w http.ResponseWriter, req *http.Request) {
 	succReply.addData("uuid", uuid)
 
 	io.WriteString(w, succReply.toJsonString())
+}
+
+func writeCard(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	if req.Method != "POST" {
+		io.WriteString(w, newReplyFailWithString("请求方法错误").toJsonString())
+		return
+	}
+
+	uuidList := req.PostForm["uuid"]
+	if len(uuidList) != 1 || len(uuidList[0]) == 0 {
+		io.WriteString(w, newReplyFailWithString("请求参数错误").toJsonString())
+		return
+	}
+
+	wsr, err := wsr.NewWsr()
+	if err != nil {
+		fmt.Println("init failed, error:", err)
+		io.WriteString(w, newReplyFail(err).toJsonString())
+		return
+	}
+	defer func() {
+		fmt.Println("destroying WSR")
+		wsr.Destroy()
+	}()
+
+	if err := wsr.OpenPort(); err != nil {
+		fmt.Println("open port failed, error:", err)
+		io.WriteString(w, newReplyFail(err).toJsonString())
+		return
+	}
+	defer func() {
+		err := wsr.ClosePort()
+		fmt.Println("close port, error:", err)
+	}()
+
+	if err := wsr.Beep(); err != nil {
+		fmt.Println("beep failed, error:", err)
+	}
+
+	if err := wsr.WriteUuid(uuidList[0]); err != nil {
+		fmt.Println("write uuid failed, err:", err)
+		io.WriteString(w, newReplyFail(err).toJsonString())
+		return
+	}
+
+	io.WriteString(w, newReplySucc().toJsonString())
 }
